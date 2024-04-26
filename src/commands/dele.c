7a05c261901542440a1c09a3fffa6daf6f904ddd
Fohz <114908868+Fohz67@users.noreply.github.com>
2024-04-26 15:25:23 +0200

    fix: dele.c

--- src/commands/dele.c
+++ src/commands/dele.c
@@ -6,39 +6,27 @@
 */
 
 #include "my_command_type.h"
-#include <sys/stat.h>
 
-static void compute_dele(client_t *client, char **args, char *path)
+void dele(client_t *client, char **args, char *path)
 {
     char *to_delete = NULL;
 
+    if (!CLIENT_OK(client) || !args || !args[1] || !path) {
+        return;
+    }
     if (args[1][0] == SLASH) {
         to_delete = build_path(path, &args[1][1], false);
     } else {
         to_delete = build_path(client->cwd, args[1], false);
     }
-    if (to_delete) {
-        if (remove(to_delete) == FAIL) {
-            SAFE_WRITE(client->socket.fd, _550);
-        } else {
-            SAFE_WRITE(client->socket.fd, _250);
-        }
-        free(to_delete);
+    if (!to_delete) {
+        SAFE_WRITE(client->socket.fd, _550);
+        return;
+    }
+    if (remove(to_delete) == FAIL) {
+        SAFE_WRITE(client->socket.fd, _550);
     } else {
-        SAFE_WRITE(client->socket.fd, _550);
+        SAFE_WRITE(client->socket.fd, _250);
     }
-}
-
-void dele(client_t *client, char **args, char *path)
-{
-    struct stat st;
-
-    if (!CLIENT_OK(client) || !args || !args[1] || !path) {
-        return;
-    }
-    if (stat(args[1], &st) == FAIL || !S_ISREG(st.st_mode)) {
-        SAFE_WRITE(client->socket.fd, _550);
-        return;
-    }
-    compute_dele(client, args, path);
+    free(to_delete);
 }
